52074fedf84d957bb10ce12fe5239079ea94aabd
mamun-prodhan <mamunpr9@gmail.com>
2023-09-15 20:51:29 +0600

    data loaded

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Blogs from "./components/Blogs/Blogs";
 import Header from "./components/Header/Header";
 
 function App() {
   return (
     <>
       <Header></Header>
+      <Blogs></Blogs>
     </>
   );
 }
